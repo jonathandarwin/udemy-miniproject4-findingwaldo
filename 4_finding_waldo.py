eacdde71bcc35ebfc01514e846bf9cd91862ef9d
jonathandarwin <jonathanjojo38@yahoo.com>
2019-06-07 18:21:36 +0700

    initial commit

---
+++ 4_finding_waldo.py
@@ -0,0 +1,20 @@
+import cv2
+import numpy as np
+
+image = cv2.imread('WaldoBeach.jpg')
+cv2.imshow('Where is waldo?', image)
+cv2.waitKey(0)
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+template = cv2.imread('waldo.jpg', 0)
+
+result = cv2.matchTemplate(gray, template, cv2.TM_CCOEFF)
+minVal, maxVal, minLoc, maxLoc = cv2.minMaxLoc(result)
+
+# creating bounding box
+topLeft = maxLoc
+bottomRight = (topLeft[0] + 50, topLeft[1] + 50)
+cv2.rectangle(image, topLeft, bottomRight, (0, 0, 255), 5)
+
+cv2.imshow('Where is waldo?', image)
+cv2.waitKey(0)
